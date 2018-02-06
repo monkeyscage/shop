db00e9a4a2a36edf84ee978c6e648f3bdf9ef439
maxxflyer <maxxflyer@gmail.com>
2018-02-07 00:07:21 +0100

    Update s.sol

--- s.sol
+++ s.sol
@@ -1,7 +1,7 @@
-contract s is superowned{
+contract s{
 address[] public input;
 address[] public output;
-
+address public owner;
 
 mapping(address => mapping(address => uint))public price;
 
@@ -16,40 +16,41 @@ input.push(_input);
 }
 
 function removeInput(uint _i)returns (bool){
-input[i]=input[input.length-1];
+input[_i]=input[input.length-1];
 input.length--;
 }
 
-function addOutput(address _input)returns (bool){
+function addOutput(address _output)returns (bool){
 output.push(_output);
 }
 
-function removeOutput(address _input)returns (bool){
-output[i]=output[output.length-1];
+function removeOutput(uint _i)returns (bool){
+output[_i]=output[output.length-1];
 output.length--;
 }
 
 function buy(address _output,address _input,uint _amount){
 if(price[_output][_input]==0)revert();
-erc20 token=erc20(_input);
-if(!token.transferFrom(msg.sender,this,_amount))revert();
+//erc20 token=erc20(_input);
+//if(!token.transferFrom(msg.sender,this,_amount))revert();
 uint total;
 if(price[_output][_input]<1000000000000000000){
 total=price[_output][_input]*_amount;}else{
 total=price[_output][_input]/1000000000000000000*_amount;}
-token=erc20(_output);
-if(!token.transfer(msg.sender,total))revert();
+//token=erc20(_output);
+//if(!token.transfer(msg.sender,total))revert();
 }
 
-function transferToken(address _to,address _token,uint _amount) onlyOwner{
-token.transfer(msg.sender,_amount);
+function transferToken(address _to,address _token,uint _amount) {
+//erc20 token=erc20(_token);
+//token.transfer(msg.sender,_amount);
 }
 
-function transferETH(address _to,uint _amount)onlyOwner{
-send(msg.sender,_amount);
+function transferETH(address _to,uint _amount){
+//send(msg.sender,_amount);
 }
 
-function inspect(uint _i)constant returns(){
+function inspect(uint _i)constant returns(address,uint,address,uint){
 return(input[_i],input.length,output[_i],output.length);
 }
 
