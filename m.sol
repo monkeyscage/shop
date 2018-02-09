8c2ff1e8e53801b8ad68082844e0ae58244c9dec
maxxflyer <maxxflyer@gmail.com>
2018-02-09 10:51:27 +0100

    Update m.sol

--- m.sol
+++ m.sol
@@ -1,17 +1,18 @@
 contract m{
 address public owner;
-
+mapping(address => bool)public certified;
 function m(){
 owner=msg.sender;
 }
 
 function buy(address _seller,address _buyer,address _output,address _input,uint _inputamount) returns(bool){
+if((!certified[_buyer])||(!certified[_seller]))revert();
 MTS buyer=MTS(_buyer);
 if(msg.sender!=buyer.owner)revert();
 MTS seller=MTS(_seller);
 if(seller.sell(_buyer,_output,_input,_inputamount)){
-if(!buyer.buynow(_input,_inputamount))revert();
+if(!buyer.buynow(_seller,_input,_inputamount))revert();
 }else{revert();}
 }
-retunr true;
+return true;
 }
