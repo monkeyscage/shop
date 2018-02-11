39751eae64c714951eaebe5a8f6f1d76ada5130d
maxxflyer <maxxflyer@gmail.com>
2018-02-11 10:46:08 +0100

    Update s.sol

--- s.sol
+++ s.sol
@@ -1,48 +1,157 @@
 pragma solidity 0.4.19;
 
+
+contract EIP20Interface {
+    /* This is a slight change to the ERC20 base standard.
+    function totalSupply() constant returns (uint256 supply);
+    is replaced with:
+    uint256 public totalSupply;
+    This automatically creates a getter function for the totalSupply.
+    This is moved to the base contract since public getter functions are not
+    currently recognised as an implementation of the matching abstract
+    function by the compiler.
+    */
+    /// total amount of tokens
+    uint256 public totalSupply;
+
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) public view returns (uint256 balance);
+
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transfer(address _to, uint256 _value) public returns (bool success);
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+
+    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of tokens to be approved for transfer
+    /// @return Whether the approval was successful or not
+    function approve(address _spender, uint256 _value) public returns (bool success);
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+
+    // solhint-disable-next-line no-simple-event-func-name  
+    event Transfer(address indexed _from, address indexed _to, uint256 _value); 
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+contract EIP20 is EIP20Interface {
+
+    uint256 constant private MAX_UINT256 = 2**256 - 1;
+    mapping (address => uint256) public balances;
+    mapping (address => mapping (address => uint256)) public allowed;
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show.
+    string public symbol;                 //An identifier: eg SBX
+
+    function EIP20(
+        uint256 _initialAmount,
+        string _tokenName,
+        uint8 _decimalUnits,
+        string _tokenSymbol
+    ) public {
+        balances[msg.sender] = _initialAmount;               // Give the creator all initial tokens
+        totalSupply = _initialAmount;                        // Update total supply
+        name = _tokenName;                                   // Set the name for display purposes
+        decimals = _decimalUnits;                            // Amount of decimals for display purposes
+        symbol = _tokenSymbol;                               // Set the symbol for display purposes
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balances[msg.sender] >= _value);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        uint256 allowance = allowed[_from][msg.sender];
+        require(balances[_from] >= _value && allowance >= _value);
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        if (allowance < MAX_UINT256) {
+            allowed[_from][msg.sender] -= _value;
+        }
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }   
+}
+
+
 contract s{
 address[] public input;
 address[] public output;
 address public owner;
 
- modifier permissioned{ if(!permissions[msg.sender])revert();
- }
+
 
 mapping(address => mapping(address => uint))public price;
 mapping(address => bool)public permissions;
 
 function s(address _owner,address _master){owner=_owner;permissions[_owner]=true;permissions[_master]=true;}
 
-function setPrice(address _output,address _input,uint _price)returns (bool) permissioned{
+function setPrice(address _output,address _input,uint _price)returns (bool){
 price[_output][_input]=_price;
 }
 
-function addInput(address _input) permissioned{
+function addInput(address _input){
 input.push(_input);
 }
 
-function removeInput(uint _i)returns (bool) permissioned{
+function removeInput(uint _i)returns (bool){
 input[_i]=input[input.length-1];
 input.length--;
 }
 
-function addOutput(address _output)returns (bool) permissioned{
+function addOutput(address _output)returns (bool){
 output.push(_output);
 }
 
-function removeOutput(uint _i)returns (bool) permissioned{
+function removeOutput(uint _i)returns (bool){
 output[_i]=output[output.length-1];
 output.length--;
 }
 
-function sell(address _buyer,address _output,address _input,uint _inputamount) returns(bool)permissioned{
-erc20 token=erc20(_output);
+function sell(address _buyer,address _output,address _input,uint _inputamount) returns(bool){
+EIP20 token=EIP20(_output);
 if(!token.transfer(msg.sender,_inputamount/price[_output][_input]))revert();
 return true;
 }
 
-function buynow(address _seller,address _input,uint _inputamount)onlymaster{
-erc20 token=erc20(_input);
+function buynow(address _seller,address _input,uint _inputamount)returns(bool){
+EIP20 token=EIP20(_input);
 if(!token.transfer(_seller,_inputamount))revert();
 return true;
 }
@@ -59,30 +168,30 @@ total=price[_output][_input]/1000000000000000000*_amount;}
 //if(!token.transfer(msg.sender,total))revert();
 }
 
-function transferToken(address _to,address _token,uint _amount) permissioned{
-erc20 token=erc20(_token);
+function transferToken(address _to,address _token,uint _amount) {
+EIP20 token=EIP20(_token);
 token.transfer(msg.sender,_amount);
 }
 
-function transferETH(address _to,uint _amount)permissioned{
-if(!msg.sender.transfer(_amount))revert();
+function transferETH(address _to,uint _amount){
+//if(!msg.sender.transfer(_amount))revert();
 }
 
 function inspect(uint _i)constant returns(uint,uint){
 return(input.length,output.length);
 }
 
-function setController(address _controller,bool active)permissioned{
+function setController(address _controller,bool active){
 permissions[_controller]=active;
 }
 
-function approve(address _user,address _token,uint _amount) permissioned{
-erc20 token=erc20(_token);
+function approve(address _user,address _token,uint _amount){
+EIP20 token=EIP20(_token);
 token.approve(_user,_amount);
 }
 
-function action(address _contract,uint _value,uint _gas,string _function,byte _byte) permissioned{
-_contract.delegatecall.gas(_gas).value(_value)(b ytes4(sha3(_function)),_byte);
+function action(address _contract,uint _value,uint _gas,string _function,byte _byte){
+//_contract.delegatecall.gas(_gas).value(_value)(bytes4(sha3(_function)),_byte);
 }
 
 
