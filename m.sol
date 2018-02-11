cfb186349108dfbd068f3624126b7a7637b93dbd
maxxflyer <maxxflyer@gmail.com>
2018-02-11 11:53:54 +0100

    Update m.sol

--- m.sol
+++ m.sol
@@ -6,13 +6,14 @@ owner=msg.sender;
 }
 
 function buy(address _seller,address _buyer,address _output,address _input,uint _inputamount) returns(bool){
-if((!certified[_buyer])||(!certified[_seller]))revert();
-MTS buyer=MTS(_buyer);
-if(msg.sender!=buyer.owner)revert();
-MTS seller=MTS(_seller);
+//if((!certified[_buyer])||(!certified[_seller]))revert();
+s buyer=s(_buyer);
+if(msg.sender!=address(buyer.owner))revert();
+s seller=s(_seller);
 if(seller.sell(_buyer,_output,_input,_inputamount)){
 if(!buyer.buynow(_seller,_input,_inputamount))revert();
 }else{revert();}
-}
+
 return true;
+}
 }
