bee94a2a60b8cd0a1a1f4ef5419e30f118f14380
maxxflyer <maxxflyer@gmail.com>
2018-02-21 02:07:11 +0100

    Create f.sol

---
+++ f.sol
@@ -0,0 +1,13 @@
+contract friendster{
+address[] public friends;
+address[] public requests;
+
+function friendster(){
+}
+
+function addfriend(address shop,address friend){
+}
+
+function removefriend(address shop,uint i){
+}
+}
