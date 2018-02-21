8ab1e321cd15a9c0e0a9b7bf0e1f2a336b1165e1
maxxflyer <maxxflyer@gmail.com>
2018-02-21 02:13:41 +0100

    Update f.sol

--- f.sol
+++ f.sol
@@ -1,13 +1,21 @@
 contract friendster{
-address[] public friends;
-address[] public requests;
+mapping(address => address[]) public friends;
+mapping(address => address[]) public requests;
 
 function friendster(){
 }
 
 function addfriend(address shop,address friend){
+s tshop=s(shop);
+if(tshop.owner()==msg.sender)friends[shop].push(friend);
 }
 
 function removefriend(address shop,uint i){
+s tshop=s(shop);
+if(tshop.owner()==msg.sender){
+friends[shop][i]=friends[shop][friends[shop].length-1];
+friends[shop].length--;
 }
+
+
 }
