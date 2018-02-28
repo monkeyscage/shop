3f448e51471bec2b7c737628d8c951f686426c51
maxxflyer <maxxflyer@gmail.com>
2018-02-28 14:31:37 +0100

    Update m.sol

--- m.sol
+++ m.sol
@@ -1,20 +1,20 @@
 pragma solidity 0.4.20;
 contract m{
 address public owner;
+mapping(address => bool)public permissions;
 mapping(address => bool)public certified;
 function m(){
 owner=msg.sender;
 }
 
 function buy(address _seller,address _buyer,address _output,address _input,uint _inputamount) returns(bool){
-//if((!certified[_buyer])||(!certified[_seller]))revert();
+if((!certified[_buyer])||(!certified[_seller]))revert();
 s buyer=s(_buyer);
 if(msg.sender!=address(buyer.owner))revert();
 s seller=s(_seller);
-if(seller.sell(_buyer,_output,_input,_inputamount)){
-if(!buyer.buynow(_seller,_input,_inputamount))revert();
+if(buyer.buynow(_seller,_input,_inputamount)){
+if(!seller.sell(_buyer,_output,_input,_inputamount))revert();
 }else{revert();}
-
 return true;
 }
 }
@@ -27,6 +27,18 @@ function upgrade(address _shop,uint i,bool active,address _controller){
     }else{
         if(u.removable(_controller))s.setController(_controller,false);
     }
+    
+ function add(address a){
+ if(!permissions[msg.sender])revert();
+ certified[a]=true;
+ }
+ 
+ function setPermission(address a,bool b){
+if(msg.sender!=owner)revert();
+permission[a]=b;
+}
+
+
 }
 
 contract s{
